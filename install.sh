3ce9368cc7bc1f169631dcacc12313c2e9abc163
tciooc <qinjiahui91@gmail.com>
2023-10-12 20:29:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "即将安装ept 0.0.1-alpha版本"
-curl https://trial-cdn.esign.cn/upload/4522d0e6-2694-5d83-b5e8-55fd4c628beb\!\!10-12.zip --output ept-bin.zip
+curl https://trial-cdn.esign.cn/upload/b4411f5e-1e20-5e1f-bf6f-f0316ba2e059!!10-13.zip --output ept-bin.zip
 unzip ept-bin.zip
 system=`uname`
 if [ "$system" == "Darwin" ]; then
@@ -12,5 +12,7 @@ else
 fi
 chmod +x /usr/local/bin/ept
 rm -rf __MACOSX ept ept-bin.zip
+echo "初始化模版数据"
+ept config set template.project https://trial-cdn.esign.cn/upload/46582824-9022-5d85-bf28-8526c7ee15ca!!10-13.zip
 echo "安装完成"
 echo "请执行ept -h查看如何使用"
